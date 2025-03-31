996b03d765cb228d802f552d1730c68325a6b398
aTramelKHS <minecraft.gamerajt@gmail.com>
2025-03-31 14:18:21 +0000

    add circularity project

---
+++ fsd-projects/other projects/menuscript.js
@@ -0,0 +1,33 @@
+const start = document.getElementById('startBtn');
+
+start.addEventListener('click', () => {
+  game();
+  document.getElementById('game').hidden = false;
+  document.getElementById('queue').hidden = false;
+  document.getElementById('UI').hidden = false;
+  //document.getElementsByClassName('settings').hidden = true;
+});
+
+
+/*
+document.getElementById('lvl1').addEventListener('click', (e) => {
+  level = 1;
+  console.log(level);
+});
+
+document.getElementById('lvl2').addEventListener('click', (e) => {
+  level = 2;
+  console.log(level);
+});
+
+document.getElementById('lvl3').addEventListener('click', (e) => {
+  level = 3;
+  console.log(level);
+});
+
+document.getElementById('lvl4').addEventListener('click', (e) => {
+  level = 4;
+  console.log(level);
+}); */
+
+
